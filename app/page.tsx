11b610bfe5fba96db6be87be4020d6f83bac1c40
Donya <donya@Donyas-MacBook-Pro.local>
2023-11-11 19:30:33 +0100

    link to sources and minor deisgn adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -19,25 +19,32 @@ export default function Chat() {
     <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
       {messages.length > 0
         ? messages.map((m, i) => (
-            <div key={m.id} className="flex flex-col mb-6">
-              <b>{m.role === "user" ? "User: " : "AI: "}</b>
+          <div key={m.id} className="flex flex-col mb-6">
+            <b>{m.role === "user" ? "You: " : "Simplaw: "}</b>
 
-              <small className="text-gray-500">
-                {parsedData?.[i]?.context
-                  ?.map(({ payload }) => payload.article)
-                  .join(", ")}
-              </small>
+            <small className="source">
+              {parsedData?.[i]?.context?.map(({ payload }, index, array) => (
+                <span key={index}>
+                  <a href={payload.link} target="_blank" rel="noopener noreferrer">
+                    {payload.article}
+                  </a>
+                  {index < array.length - 1 && ' | '}
+                </span>
+              ))}
+            </small>
 
-              <p className="whitespace-pre-wrap">{m.content.trim()}</p>
-            </div>
-          ))
+
+            <p className="whitespace-pre-wrap">{m.content.trim()}</p>
+
+          </div>
+        ))
         : null}
 
       <form onSubmit={handleSubmit}>
         <input
           className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2"
           value={input}
-          placeholder="Say something..."
+          placeholder="Ask me anything..."
           onChange={handleInputChange}
         />
       </form>
