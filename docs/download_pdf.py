7ba27a10f21c3730e6d2fedc33bc65f35b717147
Jenifer Tabita Ciuciu-Kiss <jenife.girl.98@gmail.com>
2023-11-12 10:05:22 +0100

    some refactoring

--- docs/download_pdf.py
+++ docs/download_pdf.py
@@ -5,9 +5,12 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import fitz  # PyMuPDF
+import json
 
 # Base URL of the site to scrape
 base_url = 'https://entscheidsuche.ch/docs/'
+result = {}
 
 
 def get_links(url):
@@ -42,19 +45,58 @@ def get_pdf_links(url):
             pdf_links.append(url+'/'+href.split('/')[-1])
     return pdf_links
 
-def download_pdf(base_url):
+def download_pdf_and_save_text(base_url):
     # Get the links from the base URL
     links = get_links(base_url)
+    row_counter = 0
     for link in links:
         pdf_links = get_pdf_links(link)
         for i, link in enumerate(pdf_links):
             print(link)
             response = requests.get(link)
-            pdf = open("pdf"+str(i)+".pdf", 'wb')
+            filename = "pdfs/pdf"+str(i)+".pdf"
+            pdf = open(filename, 'wb')
             pdf.write(response.content)
             pdf.close()
+            texts = extract_text_from_pdf(filename)
+            for text in texts:
+                text = text.replace('\n', ' ')
+                if text == "":
+                    continue
+                result[row_counter] = {"article": text,
+                                       "link": link,
+                                       "headings": [],
+                                       "content": "",
+                                       "contentHTML": ""}
+                row_counter += 1
             print("File ", i, " downloaded")
+            with open('data.json', 'w') as f:
+                f.write(json.dumps(result, indent=4))
 
 
-download_pdf(base_url)
+def extract_text_from_pdf(pdf_path):
+    # Open the provided PDF file
+    pdf_document = fitz.open(pdf_path)
+    extracted_texts = []
+
+    # Iterate over each page in the PDF
+    for page_number in range(len(pdf_document)):
+        # Get the page
+        page = pdf_document[page_number]
+
+        # Extract text from the page
+        text = page.get_text()
+
+        # Append the text to our list
+        extracted_texts.append(text)
+
+    # Close the PDF after extraction
+    pdf_document.close()
+    return extracted_texts
+
+
+download_pdf_and_save_text(base_url)
+
+
+
 
